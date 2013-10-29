ed36e418a00f4bc85de1313a54ca6c26810285a7
Matteo Figus <matteofigus@gmail.com>
2013-10-29 13:23:13 +0000

    Got the first part working

--- test/http-test-servers.js
+++ test/http-test-servers.js
@@ -1,4 +1,5 @@
 var should = require('should');
+var superagent = require('superagent');
 var TestServers = require('./../lib/http-test-servers');
 var _ = require('underscore');
 
@@ -23,11 +24,28 @@ describe('http test servers', function(){
     var testServers = new TestServers(endpoints, servers);
 
     testServers.start(function(testServers){
+
+
       testServers.should.not.be.empty;
+      testServers.endpoints.route1.should.be.eql('/getData');
+      testServers.endpoints.route2.should.be.eql('/getData2');
       testServers.servers.server1.port.should.be.eql(3006);
       testServers.servers.server2.port.should.be.eql(3007);
 
-      testServers.kill(done);
+
+      superagent.get("http://localhost:3006/getData", function(response){
+        response.statusCode.should.be.eql(200);
+        response.body.should.be.eql({ message: 'route1'});
+
+        superagent.get("http://localhost:3007/getData2", function(response2){
+          response2.statusCode.should.be.eql(200);
+          response2.body.should.be.eql({ message: 'route2'});
+          testServers.kill(done);
+        });
+
+
+      });
+
     });
 
   });
